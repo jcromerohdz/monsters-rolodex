42b0a112c1e7b57bf462fb063eac1a8692a43441
jcromerohdz <jcromerohdz@gmail.com>
2020-03-09 14:57:12 -0700

    Updates

---
+++ precache-manifest.ffc1086a7cae0b008c3d38728c2f3fb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b5b25aa1d915037924344e466a713a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "711040e79a4c16f1a904",
+    "url": "/monsters-rolodex/static/css/main.dbb4d3e5.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "711040e79a4c16f1a904",
+    "url": "/monsters-rolodex/static/js/main.e9411c24.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
